789af8734fa382096129c538968dcf5e585f0e33
Henry Li <henry1943@163.com>
2018-09-01 11:00:37 +0800

    Add constants.h

---
+++ src/constants.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define SERVER_NAME_PREFIX "LightroomMate"
+
+#define MIDI_SERVICE_UUID "03b80e5a-ede8-4b33-a751-6ce34ec4c700"
+#define MIDI_CHARACTERISTIC_UUID "7772e5db-3868-4112-a1a9-f2669d106bf3"
